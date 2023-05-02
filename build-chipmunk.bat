7cf5f2c61ca6a6baf3179c3a75494f2581a83281
evn <gh@cli.lol>
2023-05-02 05:35:21 +0200

    :eye:

--- build-chipmunk.bat
+++ build-chipmunk.bat
@@ -4,5 +4,5 @@ rmdir
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -S. -Bbuild -DBUILD_STATIC=ON -DBUILD_SHARED=OFF -DBUILD_DEMOS=OFF -DCMAKE_TOOLCHAIN_FILE=/c/devkitPro/cmake/3DS.cmake ../chipmunk
 cmake --build build
-xcopy /s .\build\src\libchipmunk.a ..\PhysGame\lib\libchipmunk.a /Y
+xcopy /s .\build\src\libchipmunk.a ..\PhysGame\lib\ /Y
 cd ..
