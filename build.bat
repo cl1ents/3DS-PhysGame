9861d3aa2d2e1a5f5a43a64ab479d3fbd786e5ed
evn <gh@cli.lol>
2023-05-02 06:07:39 +0200

    Best build script ever

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
+@echo off
 cd PhysGame
 make cia
 cd ..
+
+echo Attempting to serve CIA over local wifi...
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+curl qrenco.de/%NetworkIP%:8000/PhysGame.cia
+echo Scan this QR code using your 3DS :3
+echo Then feel free to CTRL+C
+python -m http.server --directory .\PhysGame\output\
