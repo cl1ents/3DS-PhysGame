dafa2fd15b207ba351b0ef6d8103a606333d461f
evⁿ ☕ <gh@cli.lol>
2023-05-03 04:51:35 +0200

    whoops

--- build-chipmunk.bat
+++ build-chipmunk.bat
@@ -1,6 +1,5 @@
 rmdir /s /q build
 mkdir build
-rmdir
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -S. -Bbuild -DBUILD_STATIC=ON -DBUILD_SHARED=OFF -DBUILD_DEMOS=OFF -DCMAKE_TOOLCHAIN_FILE=/c/devkitPro/cmake/3DS.cmake ../chipmunk
 cmake --build build
